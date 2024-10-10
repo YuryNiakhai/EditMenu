b2b1f91e28386eaca1707599fdbc77c19de2a2ce
Yury Niakhai <80161944+YuryNiakhai@users.noreply.github.com>
2024-10-10 17:50:32 +0300

    wip

--- Sources/EditMenu/EditMenuView.swift
+++ Sources/EditMenu/EditMenuView.swift
@@ -89,7 +89,7 @@ public struct EditMenuView<Content: View>: UIViewControllerRepresentable {
             }
             
             // show the menu from the root view
-            let targetView = view.subviews.first(where: { $0.tag == -333 }) ?? view.subviews.first
+            let targetView = view.subviews.first(where: { String(describing: $0).lowercased().contains("drawing") }) ?? view.subviews.first
             let validatedTargetViee = targetView ?? view
             menu.showMenu(from: validatedTargetViee, rect: validatedTargetViee.bounds)
         }
