e716b3147655efa6b5f6481adb8eed1fae212dea
Yury Niakhai <80161944+YuryNiakhai@users.noreply.github.com>
2024-11-14 15:39:39 +0200

    Update EditMenuView.swift

--- Sources/EditMenu/EditMenuView.swift
+++ Sources/EditMenu/EditMenuView.swift
@@ -159,7 +159,7 @@ public struct EditMenuView<Content: View>: UIViewControllerRepresentable {
         override func viewDidLoad() {
             super.viewDidLoad()
 
-            var expectedSize = view.systemLayoutSizeFitting(.init(width: 300, height: CGFloat.infinity))
+            var expectedSize = view.systemLayoutSizeFitting(.init(width: parent!.view.frame.width - 40, height: CGFloat.infinity))
             expectedSize.height += 1
             preferredContentSize = expectedSize
         }
