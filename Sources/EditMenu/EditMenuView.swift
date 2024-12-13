3c5c5bd78e8b93b9e06c171d0857dd5387819c85
Yury Niakhai <80161944+YuryNiakhai@users.noreply.github.com>
2024-12-13 16:16:21 +0200

    Update EditMenuView.swift

--- Sources/EditMenu/EditMenuView.swift
+++ Sources/EditMenu/EditMenuView.swift
@@ -31,7 +31,7 @@ public struct EditMenuItem {
 public extension View {
     /// Attaches a long-press action to this `View` withe the given item titles & actions
     public func editMenu(@ArrayBuilder<EditMenuItem> _ items: () -> [EditMenuItem] = { [EditMenuItem]() }, copyHandler: (() -> Void)?, responderHandler: ((UIResponder?) -> Void)?) -> some View {
-        EditMenuView(content: self, items: items(), copyHandler: copyHandler, responderHandler: responderHandler)
+        EditMenuView(content: self, items: items(), copyHandler: copyHandler, maxWidth: maxWidth, responderHandler: responderHandler)
     }
 }
 
